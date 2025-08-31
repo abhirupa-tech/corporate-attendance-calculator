3640ce689b260d62d4f4320940faee91ac84d774
Abhirupa Mitra <mitra.abhirupa@gmail.com>
2025-08-31 17:02:02 +0530

    Final changes

--- src/components/attendanceTracker.tsx
+++ src/components/attendanceTracker.tsx
@@ -4,7 +4,7 @@ import BentoGrid from "./bentoGrid";
 const AttendanceTracker: React.FC = () => {
   return (
     <React.StrictMode>
-      <p className="text-3xl p-5 bg-black/50 sm:bg-transparent sm:text-5xl lg:text-4xl [font-family:'Cormorant_Garamond',serif] mb-0 sm:mb-6 lg:mb-0 w-full sm:w-auto">
+      <p className="text-xl sm:text-3xl p-5 bg-black/50 sm:bg-transparent md:text-5xl lg:text-4xl [font-family:'Cormorant_Garamond',serif] mb-0 sm:mb-6 lg:mb-0 w-full sm:w-auto">
         Calculate Your{" "}
         <span className=" italic [font-variation-settings:'ital'_5]">
           Attendance
@@ -13,8 +13,8 @@ const AttendanceTracker: React.FC = () => {
       </p>
 
       <BentoGrid />
-      <div className="max-w-[1000px] text-center px-8">
-        <p className="italic text-sm text-gray-400">
+      <div className="max-w-[1000px] text-center px-2 sm:px-8">
+        <p className="italic text-[12px] sm:text-sm text-gray-400">
           Disclaimer: Data is stored in your browser's cache. Please bookmark this page to return to your saved progress.
         </p>
       </div>
